2f48804679b577f00c953c962cd625c7b47a9785
aboudHasan <aboudhasan06@gmail.com>
2025-11-18 23:37:59 -0500

    added packet receiving and sending in the websocket logic

--- Source/Scripts/websocket.gd
+++ Source/Scripts/websocket.gd
@@ -1,16 +1,16 @@
 extends Node
 
-var WEBSOCKET_URL = Util.server_conf.get_value("NETWORK", "WEBSOCKET_PORT")
-
+var WEBSOCKET_URL: String
 var _client = WebSocketPeer.new()
 var _opened = false
+var _userID = "Player" + OS.get_unique_id()
 
 func _ready():
-	print("attempting to connect")
+	WEBSOCKET_URL = Util.server_conf.get_value("NETWORK", "WEBSOCKET_PORT")
+	WEBSOCKET_URL += "/?user=" + _userID
+	print("attempting to connect to %s", WEBSOCKET_URL)
 	var res = _client.connect_to_url(WEBSOCKET_URL)
-	if !res.ok:
-		print("problem with connection: %s", res)
-		return
+	assert(res == OK, "connect_to_host() failed")
 	
 func _process(_delta: float) -> void:
 	_client.poll()
@@ -19,6 +19,14 @@ func _process(_delta: float) -> void:
 		if _opened == false:
 			print("connected")
 			_opened = true
+			#send_text works completely fine for emitting new message
+			_client.send_text("hello testing from godot")
+			
+		while (_client.get_available_packet_count() > 0):
+			print("new packet")
+			# get_available_packet_count() returns the number of packets available to be processed,
+			# so while it's greater than 0, print packet converted to a string
+			print(_client.get_packet().get_string_from_utf8())
 			
 	elif _client.get_ready_state() == WebSocketPeer.STATE_CLOSING:
 		pass
