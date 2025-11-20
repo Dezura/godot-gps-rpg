f7e5a90ec593f05bceeca5bbb1e14a9562bc75d7
aboudHasan <aboudhasan06@gmail.com>
2025-11-20 16:06:01 -0500

    added pause menu

--- Source/Scripts/main.gd
+++ Source/Scripts/main.gd
@@ -12,6 +12,10 @@ var player_coords := GeoCoordinate.new()
 
 
 func _ready() -> void:
+	
+	var pauseButton = $CanvasLayer/PlayerHUD/PauseButton
+	pauseButton.pressed.connect($CanvasLayer/PauseMenu.open_menu)
+
 	set_process(false)
 	android_gps.cooridnates_fetched.connect(_on_cooridnates_fetched)
 	
@@ -44,7 +48,7 @@ func _ready() -> void:
 	map_renderer.queue_render_pois("Hamilton,Ontario")
 	
 	set_process(true)
-
+	
 
 func _process(_delta: float) -> void:
 	if Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") != Vector2.ZERO:
